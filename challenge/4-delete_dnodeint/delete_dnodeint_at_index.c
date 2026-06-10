79106da5ad2a0d4fbdfad84c7f7fd52d0087257f
XCyber888 <xalidyusifov744@gmail.com>
2026-06-10 18:42:43 +0000

    Fix Task 4: Correct pointer reassignment in delete_dnodeint_at_index

---
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes the node at index of a dlistint_t linked list
+ * @head: double pointer to the head of the list
+ * @index: index of the node to delete
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+    dlistint_t *saved_head = *head;
+    unsigned int i = 0;
+
+    if (*head == NULL)
+        return (-1);
+
+    while (saved_head != NULL && i < index)
+    {
+        saved_head = saved_head->next;
+        i++;
+    }
+
+    if (saved_head == NULL)
+        return (-1);
+
+    if (saved_head == *head)
+    {
+        *head = saved_head->next;
+        if (*head != NULL)
+            (*head)->prev = NULL;
+    }
+    else
+    {
+        saved_head->prev->next = saved_head->next;
+        if (saved_head->next != NULL)
+            saved_head->next->prev = saved_head->prev;
+    }
+
+    free(saved_head);
+    return (1);
+}
